10786e19d21c88ac12ee3c35a9a5582ccc2d4e6e
Florian Krönert <Florian.Kroenert@silflo.de>
2019-12-31 07:25:09 +0100

    Incorporating CRM styling

--- src/web/components/Lane.tsx
+++ src/web/components/Lane.tsx
@@ -13,9 +13,9 @@ export const Lane = (props: LaneProps) => {
 
     return (
         <div style={{ minWidth: "200px", margin: "5px", flex: "1 1 0" }}>
-            <Card style={{borderColor: "#d8d8d8", borderTopColor: "#3b79b7", borderTopWidth: "3px"}}>
+            <Card style={{borderColor: "#d8d8d8", borderTopColor: "#3b79b7", borderTopWidth: "3px", color: "#333333"}}>
                 <Card.Body>
-                    <Card.Title>{props.lane.option.Label.UserLocalizedLabel.Label}</Card.Title>
+                    <Card.Title style={{color: "#045999"}}>{props.lane.option.Label.UserLocalizedLabel.Label}</Card.Title>
                     { props.lane.data.map(d => <Tile key={`tile_${d[appState.metadata.PrimaryIdAttribute]}`} data={d} />) }
                 </Card.Body>
             </Card>
